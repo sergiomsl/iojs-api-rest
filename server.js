fd73deac9255eb356eeb134ef49fa4c0c208ba63
Sergio Segovia <sergio@voilaservicios.com>
2015-09-29 02:07:13 -0500

    First Commit
    Basic API REST

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict'
+
+import http from 'http'
+import mongoose from 'mongoose'
+import router from './lib/router'
+
+const server    = http.createServer()
+const port      = process.env.PORT || 3000
+const database  = process.env.MONGO_URL || 'mongodb://localhost/directory'
+
+// DB Connection
+mongoose.connect(database, onDBConnect)
+
+server.on('request', router)
+server.on('listening', onListening)
+
+function onDBConnect (err, res) {
+  if (err) console.log(`ERROR: on connecting to database, ${err}`)
+  else {
+    console.log(`Connection established to Database`)
+    server.listen(port)
+  }
+}
+
+function onListening () {
+  console.log(`Server listening on http://localhost/${port}`)
+}
